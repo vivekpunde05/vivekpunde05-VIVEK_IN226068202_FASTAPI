b5a11248fadb90e5e40d950600267ebfd7dfedf3
Vivek Punde <vivekpunde6@gmail.com>
2026-03-16 10:19:08 +0530

    add assignment 4

--- Assignment_3/main.py
+++ Assignment_3/main.py
@@ -20,18 +20,13 @@ class OrderRequest(BaseModel):
     category: str  = Field(..., min_length=2)
     in_stock: bool = True
 products = [
-    {'id': 1, 'name': 'Wireless Mouse', 'price': 499, 'category': 'Electronics', 'in_stock': True},    {'id': 2, 'name': 'Notebook',       'price':  99, 'category': 'Stationery',  'in_stock': True},
-
+    {'id': 1, 'name': 'Wireless Mouse', 'price': 499, 'category': 'Electronics', 'in_stock': True},    
+    {'id': 2, 'name': 'Notebook',       'price':  99, 'category': 'Stationery',  'in_stock': True},
     {'id': 3, 'name': 'USB Hub',        'price': 799, 'category': 'Electronics', 'in_stock': False},
-
     {'id': 4, 'name': 'Pen Set',        'price':  49, 'category': 'Stationery',  'in_stock': True},
 
 ]
-
- 
-
 orders        = []
-
 order_counter = 1
 
 
